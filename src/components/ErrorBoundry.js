ec81e79772778dc1085de152306b13fa5ebef26d
Stefan Dimitrov <51791015+St-Dimitrov@users.noreply.github.com>
2020-04-20 06:54:50 +0300

    trying to implement working error system

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,18 @@
+import React, { component, Component } from 'react';
+
+class ErrorBoundry extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+    render(){
+        if(this.state.hasError) {
+            return <h1>Oooops. That is not good</h1>
+        }
+        return this.props.children
+    }
+}
+
+export default ErrorBoundry
